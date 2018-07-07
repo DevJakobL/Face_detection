c4a620eb28f4f238677869aaeba9e0de1c26c2e5
Jakob Lambert <jakob.lambert@dreamit.de>
2018-07-07 14:37:33 +0200

    add tensorbox to Projekt

--- TENSORBOX/test_production.py
+++ TENSORBOX/test_production.py
@@ -1,10 +1,10 @@
-import imageio as imageio
 import tensorflow as tf
 import os
 import json
 import subprocess
+from scipy.misc import imread, imresize
 from scipy import misc
- 
+
 from train import build_forward
 from utils.annolist import AnnotationLib as al
 from utils.train_utils import add_rectangles, rescale_boxes
@@ -32,11 +32,11 @@ def load_frozen_graph(frozen_graph_filename):
 
     with tf.Graph().as_default() as graph:
         tf.import_graph_def(
-            graph_def, 
-            input_map=None, 
-            return_elements=None, 
-            name="", 
-            op_dict=None, 
+            graph_def,
+            input_map=None,
+            return_elements=None,
+            name="",
+            op_dict=None,
             producer_op_list=None
         )
     return graph
@@ -51,7 +51,7 @@ def get_results(args, H, data_dir):
         new_saver = tf.train.import_meta_graph(args.graphfile)
     NUM_THREADS = 8
     with tf.Session(config=tf.ConfigProto(intra_op_parallelism_threads=NUM_THREADS),
-            graph=graph if args.frozen_graph else None) as sess:
+                    graph=graph if args.frozen_graph else None) as sess:
         sess.run(tf.global_variables_initializer())
         if args.frozen_graph:
             x_in = graph.get_tensor_by_name('x_in:0')
@@ -62,10 +62,10 @@ def get_results(args, H, data_dir):
             x_in = tf.get_collection('placeholders')[0]
             pred_boxes, pred_confidences = tf.get_collection('vars')
             #freeze_graph.freeze_graph("overfeat.pb", "", False, args.weights, "add,Reshape_2", "save/restore_all",
-             #"save/Const:0", "overfeat_frozen.pb", False, '') 
+            #"save/Const:0", "overfeat_frozen.pb", False, '')
 
         pred_annolist = al.AnnoList()
-        
+
         included_extenstions = ['jpg', 'bmp', 'png', 'gif']
         image_names = [fn for fn in os.listdir(args.datadir) if any(fn.lower().endswith(ext) for ext in included_extenstions)]
         image_dir = get_image_dir(args)
@@ -73,12 +73,12 @@ def get_results(args, H, data_dir):
         for i in range(len(image_names)):
             image_name = image_names[i]
             if H['grayscale']:
-                orig_img = imageio.imread('%s/%s' % (data_dir, image_name), mode = 'RGB' if random.random() < H['grayscale_prob'] else 'L')
+                orig_img = imread('%s/%s' % (data_dir, image_name), mode = 'RGB' if random.random() < H['grayscale_prob'] else 'L')
                 if len(orig_img.shape) < 3:
                     orig_img = cv2.cvtColor(orig_img, cv2.COLOR_GRAY2RGB)
             else:
-                orig_img = imageio.imread('%s/%s' % (data_dir, image_name), mode = 'RGB')
-            img = cv2.resize(orig_img, (H["image_height"], H["image_width"]))
+                orig_img = imread('%s/%s' % (data_dir, image_name), mode = 'RGB')
+            img = imresize(orig_img, (H["image_height"], H["image_width"]), interp='cubic')
             feed = {x_in: img}
             start_time = time()
             (np_pred_boxes, np_pred_confidences) = sess.run([pred_boxes, pred_confidences], feed_dict=feed)
@@ -92,7 +92,7 @@ def get_results(args, H, data_dir):
             pred_anno.imagePath = os.path.abspath(data_dir)
             pred_anno = rescale_boxes((H["image_height"], H["image_width"]), pred_anno, orig_img.shape[0], orig_img.shape[1], test=True)
             pred_annolist.append(pred_anno)
-           
+
             imname = '%s/%s' % (image_dir, os.path.basename(image_name))
             misc.imsave(imname, new_img)
             if i % 25 == 0:
@@ -118,5 +118,5 @@ if __name__ == '__main__':
     with open(hypes_file, 'r') as f:
         H = json.load(f)
     expname = '_' + args.expname  if args.expname else ''
- 
-    pred_annolist = get_results(args, H, os.path.dirname(args.datadir))
+
+pred_annolist = get_results(args, H, os.path.dirname(args.datadir))
