8aa20e130b1a51357d81dc76994ddd85c714d85a
Jakob Lambert <jakob.lambert@dreamit.de>
2018-09-03 19:10:21 +0200

    create wider-test automatisch add doku

--- wider_parser.py
+++ wider_parser.py
@@ -5,23 +5,19 @@ from wider import WIDER
 # arg1: path to label
 # arg2: path to images
 # arg3: label file name
-"""
-    wider_val = WIDER(os.getcwd() + '/data/wider_face_split',
+wider_val = WIDER(os.getcwd() + '/data/wider_face_split',
                   os.getcwd() + '/data/WIDER_val/images',
                   'wider_face_val.mat')
 
 wider_train = WIDER(os.getcwd() + '/data/wider_face_split',
                     os.getcwd() + '/data/WIDER_train/images',
                     'wider_face_train.mat')
-"""
 
-wider_test = WIDER(os.getcwd() + '/data/wider_face_split',
-                   os.getcwd() + '/data/WIDER_test/images',
-                   'wider_face_test.mat')
-
+tdata = []
 
 # press ctrl-C to stop the process
 def cratejson(input, output):
+    i = 0
     jdata = []
     for data in input.next():
         bdata = []
@@ -30,21 +26,30 @@ def cratejson(input, output):
                           "x2": bbox[2],
                           "y1": bbox[1],
                           "y2": bbox[3]})
-        jdata.append({"image_path": data.image_name, "rects": bdata})
+        if(i%3==0):
+            tdata.append({"image_path": data.image_name, "rects": bdata})
+        else:
+            jdata.append({"image_path": data.image_name, "rects": bdata})
+        i+=1
 
     with open(output, 'w') as f:
         f.write(json.dumps(jdata, indent=4))
 
 
 def train():
-#    cratejson(wider_train, 'data/wider_train.json')
+    cratejson(wider_train, 'data/wider_train.json')
     print "ende train"
 
 
 def val():
-#    cratejson(wider_val, 'data/wider_val.json')
+    cratejson(wider_val, 'data/wider_val.json')
     print "ende val"
+def test():
+    with open("data/wider_test.json", 'w') as f:
+        f.write(json.dumps(tdata, indent=4))
+        print "ende test"
 
 
 train()
 val()
+test()
